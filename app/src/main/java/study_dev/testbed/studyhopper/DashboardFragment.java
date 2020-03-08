02bfc249713ff30854272c6ddec21b2d1dcc2b06
mkaburis <mkaburis@gmail.com>
2020-03-08 16:27:05 -0400

    added recyclerView layout for study groups

--- app/src/main/java/study_dev/testbed/studyhopper/DashboardFragment.java
+++ app/src/main/java/study_dev/testbed/studyhopper/DashboardFragment.java
@@ -1,7 +1,9 @@
 package study_dev.testbed.studyhopper;
 
 import android.annotation.SuppressLint;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,9 +11,22 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class DashboardFragment extends Fragment {
+
+    private static final String TAG = "DashboardFragment";
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     private TextView welcomeMsg;
 
@@ -20,12 +35,30 @@ public class DashboardFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        ArrayList<studyGroupItem> studyGroupList = new ArrayList<>();
+        studyGroupList.add(new studyGroupItem(R.drawable.ic_study_group_color, "Mobile Devices", "COP 4656"));
+        studyGroupList.add(new studyGroupItem(R.drawable.ic_group_color_2, "Automata Fun", "COT 4210"));
+        studyGroupList.add(new studyGroupItem(R.drawable.ic_group_color_3, "How to not go to Jail", "CIS 4250"));
+
+
+
         View v = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        //recyclerView = v.getRootView().findViewById(R.id.study_group_list);
         welcomeMsg = v.getRootView().findViewById(R.id.welcome_txt);
-        welcomeMsg.setText("Welcome JPapi");
+        mRecyclerView = v.getRootView().findViewById(R.id.study_group_recycler_view);
+        mRecyclerView.setHasFixedSize(true); //ONLY for FIXED Size recylcerView remove later
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mAdapter = new StudyGroupAdapter(studyGroupList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        welcomeMsg.setText("Welcome Jose-Pablo Mantilla");
+
+
         return v;
 
-
     }
+
 
 }
