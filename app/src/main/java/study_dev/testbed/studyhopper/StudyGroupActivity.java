30b7dff1cff6a06ba2b8032df2985bb66fc00a0f
mkaburis <mkaburis@gmail.com>
2020-03-12 12:35:55 -0400

    Adjusted study group activity to empty activity

---
+++ app/src/main/java/study_dev/testbed/studyhopper/StudyGroupActivity.java
@@ -0,0 +1,14 @@
+package study_dev.testbed.studyhopper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudyGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_group);
+    }
+}
