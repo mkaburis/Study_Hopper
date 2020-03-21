6e77c06b707c3144bd723616f631b922e57170f4
mkaburis <mkaburis@gmail.com>
2020-03-20 21:26:30 -0400

    Added line graph to group activity

--- app/src/main/java/study_dev/testbed/studyhopper/StudyGroupActivity.java
+++ app/src/main/java/study_dev/testbed/studyhopper/StudyGroupActivity.java
@@ -2,13 +2,63 @@ package study_dev.testbed.studyhopper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+import java.util.ArrayList;
+
 public class StudyGroupActivity extends AppCompatActivity {
+
+    private static final String TAG = "StudyGroupActivity";
+
+    private LineChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_group);
+
+        mChart = findViewById(R.id.lineChart);
+
+//        mChart.setOnChartGestureListener(StudyGroupActivity.this);
+//        mChart.setOnChartValueSelectedListener(StudyGroupActivity.this);
+
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(false);
+
+        ArrayList<Entry> yValues = new ArrayList<>();
+
+        yValues.add(new Entry(0, 60f));
+        yValues.add(new Entry(1, 50f));
+        yValues.add(new Entry(2, 70f));
+        yValues.add(new Entry(3, 30f));
+        yValues.add(new Entry(4, 50f));
+        yValues.add(new Entry(5, 60f));
+        yValues.add(new Entry(6, 65f));
+
+
+
+        LineDataSet set1 = new LineDataSet(yValues, "Data Set 1");
+
+        set1.setFillAlpha(110);
+        set1.setColor(Color.RED);
+        set1.setLineWidth(3f);
+        set1.setValueTextSize(10f);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+
+        dataSets.add(set1);
+
+        LineData data = new LineData(dataSets);
+
+        mChart.setData(data);
     }
 }
