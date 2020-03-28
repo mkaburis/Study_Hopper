f6d896e65d865338ace16b6d431373c321df4cb6
Jose-Pablo <jmantilla1@mail.usf.edu>
2020-03-28 14:19:14 -0400

    added X button to class Item and onClick to recycleView
    Signed-off-by: Jose-Pablo <jmantilla1@mail.usf.edu>

--- app/src/main/java/study_dev/testbed/studyhopper/ui/profile/ClassesListAdapter.java
+++ app/src/main/java/study_dev/testbed/studyhopper/ui/profile/ClassesListAdapter.java
@@ -2,6 +2,7 @@ package study_dev.testbed.studyhopper.ui.profile;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -9,11 +10,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+import study_dev.testbed.studyhopper.R;
 import study_dev.testbed.studyhopper.studyGroupItem;
 
 public class ClassesListAdapter extends RecyclerView.Adapter<ClassesListAdapter.ClassesListViewHolder> {
 
     private ArrayList<studyGroupItem> mClassList;
+    private OnItemClickListener mListener;
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mListener = listener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
 
     @NonNull
     @Override
@@ -36,10 +47,31 @@ public class ClassesListAdapter extends RecyclerView.Adapter<ClassesListAdapter.
         public TextView mClassSubject;
         public TextView mClassNumber;
         public TextView mClassSection;
+        public ImageButton mDeleteClassButton;
 
-        public ClassesListViewHolder(@NonNull View itemView) {
+        public ClassesListViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
             super(itemView);
             // make card view for to make each class
+            mClassName = itemView.findViewById(R.id.courseName);
+            mClassSubject = itemView.findViewById(R.id.courseSubject);
+            mClassNumber = itemView.findViewById(R.id.courseNumber);
+            mClassSection = itemView.findViewById(R.id.courseSection);
+            mDeleteClassButton = itemView.findViewById(R.id.deleteClassButton);
+
+            // add onclick to delete class
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
         }
     }
 }
