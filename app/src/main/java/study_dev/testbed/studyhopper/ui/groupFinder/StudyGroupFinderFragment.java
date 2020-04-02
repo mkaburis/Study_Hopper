bde8f36d00eb04060b6d09c36a05ccf32a00102b
Jose-Pablo <jmantilla1@mail.usf.edu>
2020-04-02 18:35:22 -0400

    Added 2 more search options to UI
    Signed-off-by: Jose-Pablo <jmantilla1@mail.usf.edu>

--- app/src/main/java/study_dev/testbed/studyhopper/ui/groupFinder/StudyGroupFinderFragment.java
+++ app/src/main/java/study_dev/testbed/studyhopper/ui/groupFinder/StudyGroupFinderFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,6 +20,7 @@ public class StudyGroupFinderFragment extends Fragment {
 
     private StudyGroupFinderViewModel mViewModel;
     private static final String ARG_SECTION_NUMBER = "section_number";
+    private Spinner AgeRangeSpinner;
 
     public static StudyGroupFinderFragment newInstance() {
         return new StudyGroupFinderFragment();
@@ -38,6 +41,8 @@ public class StudyGroupFinderFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_study_group_finder, container, false);
         TextView noResultsText = v.getRootView().findViewById(R.id.NoResultsText);
         RecyclerView resultViewer = v.getRootView().findViewById(R.id.ResultRecycleViewer);
+        AgeRangeSpinner = v.getRootView().findViewById(R.id.AgeRangeSpinner);
+        fillGroupPreferencesSpinner();
 
         if (resultViewer.getChildCount() == 0) {
             noResultsText.setVisibility(View.VISIBLE);
@@ -51,6 +56,14 @@ public class StudyGroupFinderFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(StudyGroupFinderViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    private void fillGroupPreferencesSpinner() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.group_size, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        AgeRangeSpinner.setAdapter(adapter);
+        //groupPreferencesSpinner.setOnItemSelectedListener(this);
     }
 
 }
