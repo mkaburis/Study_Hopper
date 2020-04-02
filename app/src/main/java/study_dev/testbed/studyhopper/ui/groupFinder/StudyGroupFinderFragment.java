1344c5bc12dc60d0aefa9a13fca432e4a8d1e2f0
Jose-Pablo <jmantilla1@mail.usf.edu>
2020-04-02 17:40:18 -0400

    Got linked finder activity to fragments/tabs & started layout for group search by category
    Signed-off-by: Jose-Pablo <jmantilla1@mail.usf.edu>

--- app/src/main/java/study_dev/testbed/studyhopper/ui/groupFinder/StudyGroupFinderFragment.java
+++ app/src/main/java/study_dev/testbed/studyhopper/ui/groupFinder/StudyGroupFinderFragment.java
@@ -4,26 +4,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import study_dev.testbed.studyhopper.R;
 
 public class StudyGroupFinderFragment extends Fragment {
 
     private StudyGroupFinderViewModel mViewModel;
+    private static final String ARG_SECTION_NUMBER = "section_number";
 
     public static StudyGroupFinderFragment newInstance() {
         return new StudyGroupFinderFragment();
     }
 
+    public static StudyGroupFinderFragment newInstance(int index) {
+        StudyGroupFinderFragment fragment = new StudyGroupFinderFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.study_group_finder_fragment, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_study_group_finder, container, false);
+        TextView noResultsText = v.getRootView().findViewById(R.id.NoResultsText);
+        RecyclerView resultViewer = v.getRootView().findViewById(R.id.ResultRecycleViewer);
+
+        if (resultViewer.getChildCount() == 0) {
+            noResultsText.setVisibility(View.VISIBLE);
+        }
+
+        return v;
     }
 
     @Override
