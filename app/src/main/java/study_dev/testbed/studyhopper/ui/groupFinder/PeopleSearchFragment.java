28f7375a18b61e19d510a5721bb530b76685e9a1
Jose-Pablo <jmantilla1@mail.usf.edu>
2020-04-09 19:31:39 -0400

    Got general gist of people search to work. Still a few bugs
    Signed-off-by: Jose-Pablo <jmantilla1@mail.usf.edu>

--- app/src/main/java/study_dev/testbed/studyhopper/ui/groupFinder/PeopleSearchFragment.java
+++ app/src/main/java/study_dev/testbed/studyhopper/ui/groupFinder/PeopleSearchFragment.java
@@ -4,20 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import study_dev.testbed.studyhopper.R;
+import study_dev.testbed.studyhopper.models.Profile;
 
 
 /**
@@ -33,6 +45,7 @@ public class PeopleSearchFragment extends Fragment {
     private ArrayList<peopleListItem> peopleList;
     private RecyclerView mPeopleRecycleView;
     private RecyclerView.LayoutManager mLayoutManager;
+    private PeopleSearchAdapter mAdapter;
 
     private TextView noResultsText;
     private EditText collegeText;
@@ -40,6 +53,8 @@ public class PeopleSearchFragment extends Fragment {
     private Spinner ageRangeSpinner;
     private Spinner genderSpinner;
     private Button searchButton;
+
+    private String userUniversity;
 
 
     public PeopleSearchFragment() {
@@ -65,6 +80,18 @@ public class PeopleSearchFragment extends Fragment {
         db = FirebaseFirestore.getInstance();
         peopleList = new ArrayList<>();
 
+        Bundle extras = getActivity().getIntent().getExtras();
+        String firestoreId = extras.getString("firestore-id");
+
+        db.collection("users").document(firestoreId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    userUniversity = task.getResult().getString("university");
+                }
+            }
+        });
+
 
         View v = inflater.inflate(R.layout.fragment_people_search, container, false);
         noResultsText = v.getRootView().findViewById(R.id.NoResultsText);
@@ -72,7 +99,127 @@ public class PeopleSearchFragment extends Fragment {
         mPeopleRecycleView = v.getRootView().findViewById(R.id.ResultRecycleViewer);
         mPeopleRecycleView.setHasFixedSize(false); //ONLY for FIXED Size recylcerView remove later
         mLayoutManager = new LinearLayoutManager(getContext());
+        mAdapter = new PeopleSearchAdapter(peopleList);
+        mPeopleRecycleView.setLayoutManager(mLayoutManager);
+        mPeopleRecycleView.setAdapter(mAdapter);
+
+        ageRangeSpinner = v.getRootView().findViewById(R.id.AgeSpinner);
+        genderSpinner = v.getRootView().findViewById(R.id.GenderSpinner);
+
+        fillGroupPreferencesSpinners();
+
+        collegeText = v.getRootView().findViewById(R.id.CollegeEditText);
+        majorText = v.getRootView().findViewById(R.id.MajorEditText);
+        searchButton = v.getRootView().findViewById(R.id.searchButton);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchPeople();
+            }
+        });
+
+        if (mPeopleRecycleView.getChildCount() == 0) {
+            noResultsText.setVisibility(View.VISIBLE);
+        }
 
         return v;
+    }
+
+    private void fillGroupPreferencesSpinners() {
+        ArrayAdapter<CharSequence> ageRangeAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.age_range_preference, android.R.layout.simple_spinner_item);
+        ageRangeAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        ageRangeSpinner.setAdapter(ageRangeAdapter);
+        //groupPreferencesSpinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> genderAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.group_preferences, android.R.layout.simple_spinner_item);
+        genderAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(genderAdapter);
+    }
+
+    public void searchPeople() {
+        CollectionReference usersRef = db.collection("users");
+        Query query = buildQuery(usersRef);
+
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    peopleList.clear();
+
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Profile group = document.toObject(Profile.class);
+
+                        String name = group.getFirstName() + " " + group.getLastName();
+                        peopleListItem listItem = new peopleListItem(name,
+                                group.getEmail(),
+                                group.getGender());
+
+                        peopleList.add(listItem);
+                    }
+                    reloadRecycler();
+                }
+            }
+        });
+    }
+
+    private Query buildQuery(CollectionReference collectionReference) {
+        Query groupQuery = collectionReference.whereEqualTo("university", userUniversity);
+
+        String major = majorText.getText().toString();
+        String college = collegeText.getText().toString();
+        String ageRange = ageRangeSpinner.getSelectedItem().toString();
+        String genderSpecific = genderSpinner.getSelectedItem().toString();
+
+        if (!major.isEmpty()) {
+            groupQuery = groupQuery.whereEqualTo("major", major);
+        }
+        if (!college.isEmpty()) {
+            groupQuery = groupQuery.whereEqualTo("college", college);
+        }
+        if (!ageRange.isEmpty()) {
+            int lowerBoundInt;
+            int upperBoundInt;
+
+            if (ageRange.equals("40+")) {
+                lowerBoundInt = 40;
+                upperBoundInt = 200;
+            } else {
+                String[] splitString = ageRange.split(" - ");
+                lowerBoundInt = Integer.parseInt(splitString[0]);
+                upperBoundInt = Integer.parseInt(splitString[1]);
+            }
+
+            Calendar calLower = Calendar.getInstance();
+            Calendar calUpper = Calendar.getInstance();
+            calLower.add(Calendar.YEAR, -lowerBoundInt);
+            calUpper.add(Calendar.YEAR, -upperBoundInt);
+
+            Date upperBound = calUpper.getTime();
+            Date lowerBound = calLower.getTime();
+
+
+            groupQuery = groupQuery.whereGreaterThan("dob", lowerBound).whereLessThan("dob", upperBound);
+        }
+        if (!genderSpecific.isEmpty()) {
+            if (genderSpecific.equals("Females Only Group"))
+                groupQuery = groupQuery.whereEqualTo("gender".toLowerCase(), "Female");
+            else if (genderSpecific.equals("Males Only Group"))
+                groupQuery = groupQuery.whereEqualTo("gender".toLowerCase(), "Male");
+        }
+
+        return groupQuery;
+    }
+
+    private void reloadRecycler() {
+        if (peopleList.size() == 0) {
+            noResultsText.setVisibility(View.VISIBLE);
+        } else {
+            noResultsText.setVisibility(View.GONE);
+            mAdapter = new PeopleSearchAdapter(peopleList);
+            mPeopleRecycleView.setAdapter(mAdapter);
+        }
     }
 }
