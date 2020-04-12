9a119d69e7b84303d6a420c60c075996c891b469
Jose-Pablo <jmantilla1@mail.usf.edu>
2020-04-12 18:48:17 -0400

    fetch data from firebase
    Signed-off-by: Jose-Pablo <jmantilla1@mail.usf.edu>

--- app/src/main/java/study_dev/testbed/studyhopper/GroupViewer.java
+++ app/src/main/java/study_dev/testbed/studyhopper/GroupViewer.java
@@ -2,16 +2,27 @@ package study_dev.testbed.studyhopper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
+import study_dev.testbed.studyhopper.models.Group;
+import study_dev.testbed.studyhopper.models.Member;
 import study_dev.testbed.studyhopper.ui.groupFinder.StudyGroupFinder;
+import study_dev.testbed.studyhopper.ui.studyGroup.MemberAdapter;
 
 public class GroupViewer extends AppCompatActivity {
 
@@ -23,8 +34,10 @@ public class GroupViewer extends AppCompatActivity {
     TextView ageRangeTextView;
     TextView noResultsText;
 
-    RecyclerView memberRecycleView;
     Button requestJoinButton;
+    RecyclerView memberRecycleView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private MemberAdapter mAdapter;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private DocumentReference groupRef;
@@ -37,8 +50,11 @@ public class GroupViewer extends AppCompatActivity {
         setContentView(R.layout.activity_group_viewer);
 
         Intent intent = getIntent();
-        groupId = intent.getStringExtra("groupId");
-        primaryUserId = intent.getStringExtra("primary-user-Id");
+        //groupId = intent.getStringExtra("groupId");
+        //primaryUserId = intent.getStringExtra("primary-user-Id");
+
+        groupId = "VzsLSaWT72kYz92PuvQd";
+        primaryUserId = "MykxE4OiK0qpBDkzF6cl";
 
         groupRef = db.collection("groups").document(groupId);
 
@@ -46,6 +62,7 @@ public class GroupViewer extends AppCompatActivity {
         setTitle("View Group Info");
 
         setUpComponents();
+        fetchFirebaseInfo();
     }
 
     public void setUpComponents() {
@@ -57,10 +74,59 @@ public class GroupViewer extends AppCompatActivity {
         ageRangeTextView = findViewById(R.id.ageRangeTextView);
         noResultsText = findViewById(R.id.noResultsText);
         memberRecycleView = findViewById(R.id.memberRecycleView);
+
+        memberRecycleView.setHasFixedSize(false);
+        mLayoutManager = new LinearLayoutManager(this);
+        memberRecycleView.setLayoutManager(mLayoutManager);
+
         requestJoinButton = findViewById(R.id.requestJoinButton);
+        // TODO add event listener
+
     }
 
     public void fetchFirebaseInfo() {
+        groupRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+
+                    if (document == null) {
+                        return;
+                    }
+
+                    Group group = document.toObject(Group.class);
+
+                    if (group == null) {
+                        return;
+                    }
+
+                    int maxSize = group.getMaxGroupMembers();
+
+                    groupNameTextView.setText(group.getGroupName());
+                    courseCodeTextView.setText(group.getCourseCode());
+                    maxSizeTextView.setText(String.valueOf(maxSize));
+                    genderTextView.setText(group.getGroupPreference());
+                    locationTextView.setText(group.getLocationPreference());
+                    ageRangeTextView.setText(group.getAgePreference());
+
+                }
+            }
+        });
+
+        Query query = groupRef.collection("members").orderBy("firstName", Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<Member> options = new FirestoreRecyclerOptions.Builder<Member>()
+                .setQuery(query, Member.class)
+                .build();
+
+        mAdapter = new MemberAdapter(options);
+        memberRecycleView.setAdapter(mAdapter);
+
+        if (mAdapter.getItemCount() == 0) {
+            noResultsText.setVisibility(View.VISIBLE);
+        }
+
     }
 
     @Override
